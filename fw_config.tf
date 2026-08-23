1836ad88d3c4f7d7e892f8b8f4ce23a25eea1ed2
mherreth <40528310+mherreth@users.noreply.github.com>
2026-08-23 18:24:39 -0500

    Add address objects for webhost-8 and webhost-9

--- fw_config.tf
+++ fw_config.tf
@@ -73,3 +73,13 @@ resource "panos_address_object" "webhost-7" {
   name = "webhost-7"
   value = "10.200.0.28/32"
 }
+
+ resource "panos_address_object" "webhost-8" {
+  name = "webhost-8"
+  value = "10.200.0.29/32"
+}
+
+resource "panos_address_object" "webhost-9" {
+  name = "webhost-9"
+  value = "10.200.0.30/32"
+}
